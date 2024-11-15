03f724b0908a9a36af2dc88c1d4d43f5361bc6d6
nhussein2026 <naser738nas@gmail.com>
2024-11-16 02:06:08 +0300

    Update ESLint to treat warnings as warnings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,12 @@
 module.exports = {
-    "env": {
-      "browser": true,
-      "es2021": true,
-      "node": true
-    },
-    "extends": [
-      "react-app",
-      "eslint:recommended"
-    ],
-    "rules": {
-      "no-unused-vars": ["warn"],  // Change to 'warn' instead of 'error'
-      "react-hooks/exhaustive-deps": ["warn"]  // Change to 'warn' instead of 'error'
-    }
-  };
-  
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["react-app", "eslint:recommended"],
+  rules: {
+    "no-unused-vars": ["warn"], // Change to 'warn' instead of 'error'
+    "react-hooks/exhaustive-deps": ["warn"], // Change to 'warn' instead of 'error'
+  },
+};
